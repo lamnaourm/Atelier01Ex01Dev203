49ddb5d0e75458a26989756513182cb64ae15e3f
lamnaourm <mohammed.lamnaour@gmail.com>
2022-09-16 17:33:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
+// Composant PHT
+// function Pht() {}
+const Pht = () => {
+  return <div className='from_control'>
+      <label htmlFor='pht'>Prix Hors taxe : </label>
+      <input type='number' id='pht' placeholder='donner votre pht ...'/>
+  </div>;
+}
+
+// Composant TVA
+const Tva = () => {
+  return <div className='from_control'>
+      <label>Taux TVA : </label>
+      <input type='number' id='tva' placeholder='donner votre tva ...'/>
+  </div>;
+}
+
+// Composant Buttons
+const Buttons = () => {
+  return <div className='from_button'>
+      <button>Calculer</button>
+      <button>Initialiser</button>
+  </div>;
+}
+
+// Composant Res
+const Res = () => {
+  return <div className='from_control'>
+      <label>Resultat : </label>
+      <input type='number' id='res' readOnly/>
+  </div>;
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Pht />
+      <Tva />
     </div>
   );
 }
