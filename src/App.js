194b3c46ea6b371d91e03864ca6b41c4ee3b14d0
lamnaourm <mohammed.lamnaour@gmail.com>
2022-09-16 17:41:16 +0100

    les autres composants

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
-import { Component } from 'react';
-import './App.css';
+import "./App.css";
 
 // Composant PHT
 // function Pht() {}
 const Pht = () => {
-  return <div className='from_control'>
-      <label htmlFor='pht'>Prix Hors taxe : </label>
-      <input type='number' id='pht' placeholder='donner votre pht ...'/>
-  </div>;
-}
+  return (
+    <div className="from_control">
+      <label htmlFor="pht">Prix Hors taxe : </label>
+      <input type="number" id="pht" placeholder="donner votre pht ..." />
+    </div>
+  );
+};
 
 // Composant TVA
 const Tva = () => {
-  return <div className='from_control'>
+  return (
+    <div className="from_control">
       <label>Taux TVA : </label>
-      <input type='number' id='tva' placeholder='donner votre tva ...'/>
-  </div>;
-}
+      <input type="number" id="tva" placeholder="donner votre tva ..." />
+    </div>
+  );
+};
 
 // Composant Buttons
 const Buttons = () => {
-  return <div className='from_button'>
+  return (
+    <div className="from_button">
       <button>Calculer</button>
       <button>Initialiser</button>
-  </div>;
-}
+    </div>
+  );
+};
 
 // Composant Res
 const Res = () => {
-  return <div className='from_control'>
-      <label>Resultat : </label>
-      <input type='number' id='res' readOnly/>
-  </div>;
-}
-function App() {
   return (
-    <div>
-      <Pht />
-      <Tva />
+    <div className="from_control">
+      <label>Resultat : </label>
+      <input type="number" id="res" readOnly />
     </div>
   );
+};
+
+const CalculPTTC = () => {
+  return (
+    <fieldset>
+      <legend>Calcul de PTTC</legend>
+      <Pht />
+      <Tva />
+      <Buttons />
+      <Res />
+    </fieldset>
+  );
+};
+function App() {
+  return <div>
+    <CalculPTTC/>
+  </div>;
 }
 
 export default App;
